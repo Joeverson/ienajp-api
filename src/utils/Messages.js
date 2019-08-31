c9090197a84fe4a7f31ba845d34d1f9c7ecdca96
Joerverson Santos <joerverson.santos@gmail.com>
2019-08-30 23:35:18 -0300

    adicionando messages

---
+++ src/utils/Messages.js
@@ -0,0 +1,115 @@
+export default {
+  user: {
+    success: {
+      edit: 'user.success.edit',
+      delete: 'user.success.delete',
+      create: 'user.success.create',
+      list: 'user.success.list',
+      resetPassword: 'user.success.resetPassword'
+    },
+    errors: {
+      userExist: 'user.errors.userExist',
+      emailInvalid: 'user.errors.emailInvalid',
+      passwordInvalid: 'user.errors.passwordInvalid',
+      nameInvalid: 'user.errors.nameInvalid',
+      existsOnlyOneAdminUser: 'user.errors.existsOnlyOneAdminUser',
+      userNameExist: 'user.errors.userNameExist',
+      userPhoneExist: 'user.errors.userPhoneExist',
+      userEmailExist: 'user.errors.userEmailExist',
+      userConfirmPasswordInvalid: 'user.errors.userConfirmPassworInvalid',
+      userNewPasswordIvalid: 'user.errors.userNewPasswordInvalid'
+    }
+  },
+  church: {
+    success: {
+      edit: 'church.success.edit',
+      delete: 'church.success.delete',
+      create: 'church.success.create',
+      list: 'church.success.list'
+    },
+    errors: {
+      nameInvalid: 'church.errors.nameInvalid',
+      churchNameExist: 'church.errors.churchNameExist'
+    }
+  },
+  consolidation: {
+    success: {
+      edit: 'consolidation.success.edit',
+      delete: 'consolidation.success.delete',
+      create: 'consolidation.success.create',
+      list: 'consolidation.success.list'
+    },
+    errors: {
+      invalidScreen: 'consolidation.error.screen.invalidScreen',
+      invalidParent: 'consolidation.error.screen.invalidParent'
+    }
+  },
+  frequency: {
+    success: {
+      edit: 'frequency.success.edit',
+      delete: 'frequency.success.delete',
+      create: 'frequency.success.create',
+      list: 'frequency.success.list'
+    },
+    errors: {
+      invalidScreen: 'frequency.error.screen.invalidScreen',
+      invalidParent: 'frequency.error.screen.invalidParent'
+    }
+  },
+  member: {
+    success: {
+      edit: 'member.success.edit',
+      delete: 'member.success.delete',
+      create: 'member.success.create',
+      list: 'member.success.list'
+    },
+    errors: {
+      invalidScreen: 'member.error.screen.invalidScreen',
+      invalidParent: 'member.error.screen.invalidParent'
+    }
+  },
+  networking: {
+    success: {
+      edit: 'networking.success.edit',
+      delete: 'networking.success.delete',
+      create: 'networking.success.create',
+      list: 'networking.success.list'
+    },
+    errors: {
+      invalidScreen: 'networking.error.screen.invalidScreen',
+      invalidParent: 'networking.error.screen.invalidParent'
+    }
+  },
+  news: {
+    success: {
+      edit: 'news.success.edit',
+      delete: 'news.success.delete',
+      create: 'news.success.create',
+      list: 'news.success.list'
+    },
+    errors: {
+      invalidScreen: 'news.error.screen.invalidScreen',
+      invalidParent: 'news.error.screen.invalidParent'
+    }
+  },
+  cell: {
+    success: {
+      edit: 'cell.success.edit',
+      delete: 'cell.success.delete',
+      create: 'cell.success.create',
+      list: 'cell.success.list'
+    },
+    errors: {
+      invalidScreen: 'cell.error.screen.invalidScreen',
+      invalidParent: 'cell.error.screen.invalidParent'
+    }
+  },
+  auth: {
+    token: {
+      noProvided: 'auth.token.noProvided',
+      tokenError: 'auth.token.tokenError',
+      malformatted: 'auth.token.malformatted',
+      invalid: 'auth.token.invalid'
+    }
+  }
+}
